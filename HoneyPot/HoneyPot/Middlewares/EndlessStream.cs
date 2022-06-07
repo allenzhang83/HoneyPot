52e0d959cf7fef9f955593041d9e06743e55b3b2
Allen Zhang <allen.zhang1983713@gmail.com>
2022-06-08 10:03:44 +1200

    initial commit - working app
    TODO:
    * dockerfile
    * readme

---
+++ HoneyPot/HoneyPot/Middlewares/EndlessStream.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace HoneyPot.Middlewares
+{
+    public class EndlessStream
+    {
+        private readonly RequestDelegate _next;
+        private const string Message = "Not Found";
+        private const int DelayMs = 5000;
+        public EndlessStream(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var outputStream = context.Response.Body;
+            while (true)
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes($"{Message}{Environment.NewLine}");
+                await outputStream.WriteAsync(bytes);
+                await Task.Delay(DelayMs);
+                await outputStream.FlushAsync();
+            }
+        }        
+    }
+}
---
+++ HoneyPot/HoneyPot/Middlewares/EndlessStreamMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace HoneyPot.Middlewares
+{
+    public static class EndlessStreamMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseEndlessStream(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<EndlessStream>();
+        }
+    }
+}
